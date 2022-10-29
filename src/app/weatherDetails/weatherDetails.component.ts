1a991107f89d0100507243583fe4b2b30ca67e0e
Harshit Anand <harshitanand283@gmail.com>
2022-10-29 18:19:14 +0530

    initial commit

--- src/app/weatherDetails/weatherDetails.component.ts
+++ src/app/weatherDetails/weatherDetails.component.ts
@@ -1,22 +1,27 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'weather-details',
-  templateUrl: './weatherDetails.component.html',
-  styleUrls: ['./weatherDetails.component.scss']
+	selector: "weather-details",
+	templateUrl: "./weatherDetails.component.html",
+	styleUrls: ["./weatherDetails.component.scss"],
 })
-
 export class WeatherDetails implements OnInit {
-  @Input() weatherData: data[];
+	@Input() weatherData: data[];
+	filteredCity: data;
+	ngOnInit() {}
 
-  ngOnInit() {
-
-  }
+	filterCity(value) {
+		const filteredData = this.weatherData.filter((city) => value.toLowerCase() == city.name.toLocaleLowerCase());
+		if (filteredData) {
+			this.filteredCity = filteredData[0];
+		}
+		console.log(this.filteredCity);
+	}
 }
 
 interface data {
-  name: string;
-  temperature: string;
-  wind: string;
-  humidity: string;
+	name: string;
+	temperature: string;
+	wind: string;
+	humidity: string;
 }
